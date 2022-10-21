c36d825a4ffee200e0f8b8ba639e3741ad9424c9
Tran McFarland Nguyen <73681436+Tranimal-N@users.noreply.github.com>
2022-10-21 15:07:40 -0700

    created boilerplate files and code

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// app.use('/', (req, res) => {
+//   return res.status(200).sendFile(path.resolve(__dirname, '../stc/client/styles.css'))});
+
+app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.resolve(__dirname, '../src/index.html'));
+  });
+  
+
+app.use('*', (req, res) => {
+    res.status(404);
+  });
+
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occured' }
+    };
+    const errorObj = Object.assign(defaultErr, err);
+    console.log(errorObj);
+    return res.status((errorObj.status)).json(errorObj.message);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
